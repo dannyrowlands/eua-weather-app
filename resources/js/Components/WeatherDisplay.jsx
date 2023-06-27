e253373d9536a8c8fee589c4ad1388faf7397b4f
dannyrowlands <dannyrowlands@users.noreply.github.com>
2023-06-27 23:37:07 +0100

    Formatted Dates and times.  Removed debug logging.

--- resources/js/Components/WeatherDisplay.jsx
+++ resources/js/Components/WeatherDisplay.jsx
@@ -1,29 +1,65 @@
-
+import dayjs from "dayjs";
 export default function WeatherDisplay(incomingData) {
- console.log('Weather DISPLAY DATA::',incomingData.weatherData)
+
+    const capitalizeFirst = str => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    };
+
+    const date = new Date(incomingData.weatherData.dt)
+    const sunrise = new Date(incomingData.weatherData.sunrise)
+    const sunset = new Date(incomingData.weatherData.sunset)
+
     return (
         <div className="border-gray-400 overflow-hidden shadow-sm sm:rounded-lg p-2 m-2 flex-wrap">
-            <div className="font-bold">Information:</div>
-            {typeof incomingData.weatherData !== 'undefined' &&
-                <ul>
-                    {Object.keys(incomingData.weatherData).map((value, index) => {
-                        let data = Object.values(incomingData.weatherData)[index];
-                        if(typeof data !== 'object') {
-                            return  <li key={index}>{value} {data}</li>
-                        }
-                    })}
-                </ul>
-            }
+            <div className="font-bold">Detailed Forecast:</div>
+            <div className="col-12">
+                Date: {dayjs(date).format('DD-MM-YYYY')}
+            </div>
+            <div className="col-12">
+                Sun Rise: {dayjs(sunrise).format('H:mm')}
+            </div>
+            <div className="col-12">
+                Sun Set: {dayjs(sunset).format('H:mm')}
+            </div>
+            <div className="col-12">
+                Temperature Day: {incomingData.weatherData.temp.day} C
+            </div>
+            <div className="col-12">
+                Temperature Evening: {incomingData.weatherData.temp.eve} C
+            </div>
+            <div className="col-12">
+                Pressure: {incomingData.weatherData.pressure}
+            </div>
+            <div className="col-12">
+                Rain: {incomingData.weatherData.rain}%
+            </div>
+            <div className="col-6">
+                Wind Speed: {incomingData.weatherData.wind_speed} mph
+            </div>
+            <div className="col-6">
+                Wind Direction: {incomingData.weatherData.wind_deg}
+            </div>
+            <div className="col-6">
+                Wind Gusts: {incomingData.weatherData.wind_gust} mph
+            </div>
+            <div className="col-6 pb-6">
+                UV Index: {incomingData.weatherData.uvi}
+            </div>
 
             {typeof incomingData.weatherData.weather !== 'undefined' &&
                 <div>
-                    <div className="font-bold">Weather:</div>
-                    <ul>
-
-                        <li>{incomingData.weatherData.weather[0].description}</li>
-
-
-                    </ul>
+                    <div className="font-bold">Outlook Summary:</div>
+                    <div className="border-gray-400 overflow-hidden shadow-sm sm:rounded-lg p-2 m-2 flex-wrap row">
+                        <div className="col-12">
+                            {capitalizeFirst(incomingData.weatherData.weather[0].main)}
+                        </div>
+                        <div className="col-12">
+                            {capitalizeFirst(incomingData.weatherData.weather[0].description)}
+                        </div>
+                        <div className="col-4">
+                            <img src={'https://openweathermap.org/img/wn/'+incomingData.weatherData.weather[0].icon+'@2x.png'}/>
+                        </div>
+                    </div>
                 </div>
             }
         </div>
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -11,29 +11,34 @@ import WeatherDisplay from "@/Components/WeatherDisplay.jsx";
 
 
 export default function Dashboard({ auth, data }) {
-console.log('DATA::',data.weather.daily[1])
-    const [weatherData, setWeatherData] = useState(data.weather);
-    const [cityName, setCityName] = useState(data.locationName);
-    const [favouriteList, setFavouriteList] = useState(data.favouriteList);
-    const [emailState, setEmailState] = useState(data.emailState);
+
+    const [weatherData, setWeatherData] = useState(data.weather)
+    const [cityName, setCityName] = useState(data.locationName)
+    const [favouriteList, setFavouriteList] = useState(data.favouriteList)
+    const [emailState, setEmailState] = useState(data.emailState)
     const [tabIndex, setTabIndex] = useState(0)
 
-    const dayList = []
+    const capitalizeFirst = str => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    };
 
-    data.weather.daily.map(
-        data => (
-            dayList.push(new Date(data.dt).toLocaleDateString('en-UK', {
-                weekday: 'short'
-            }))
-        ))
+    let dayList = ['Mon','Tue','Wed','Thurs','Fri','Sat','Sun','Mon']
+
+    if(typeof weatherData.daily !== 'undefined') {
+        let dayList = []
+        weatherData.daily.map(
+            data => (
+                dayList.push(new Date(data.dt).toLocaleDateString('en-UK', {
+                    weekday: 'short'
+                }))
+            ))
+    }
 
     useEffect(() => {
-        console.log("loaded");
         getList()
     }, []);
 
     useEffect(() => {
-        console.log("tabIndex changed");
     }, [tabIndex]);
 
     function getList()
@@ -43,7 +48,6 @@ console.log('DATA::',data.weather.daily[1])
         }
         axios.get('api/favourites?user_id='+auth.user.id, null, config)
             .then(response => {
-                console.log('FAVOURITES LIST::',response.data)
                 setFavouriteList(response.data)
             })
             .catch(error => {
@@ -56,7 +60,6 @@ console.log('DATA::',data.weather.daily[1])
         event.preventDefault();
 
         const formData = new FormData(event.target);
-        console.log(formData.get('city'))
         const config = {
             headers: { 'content-type': 'multipart/form-data' }
         }
@@ -65,7 +68,6 @@ console.log('DATA::',data.weather.daily[1])
 
         axios.get('api/weather/'+city, formData, config)
             .then(response => {
-                console.log(response.data)
                 setCityName(city)
                 setWeatherData(response.data)
             })
@@ -77,25 +79,21 @@ console.log('DATA::',data.weather.daily[1])
     function addFavourite(event) {
         // Prevent default behavior
         event.preventDefault();
-        console.log('ADD FAVOURITE')
-        const formData = new FormData(event.target);
-        console.log(formData.get('favourite'))
+        const formData = new FormData(event.target)
         const config = {
             headers: { 'content-type': 'multipart/form-data' }
         }
 
         axios.post('api/favourite', formData, config)
             .then(response => {
-                getList();
-                console.log(response.data)
+                getList()
             })
             .catch(error => {
-                console.log(error);
+                console.log(error)
             });
     }
 
     function handleToggleEmail() {
-        console.log('handleToggleEmail')
 
         const config = {
             headers: { 'content-type': 'multipart/form-data' }
@@ -105,14 +103,13 @@ console.log('DATA::',data.weather.daily[1])
             new_state: !emailState,
             user_id: auth.user.id
         }
-console.log('PARAMS',params)
+
         axios.post('api/email-toggle', params, config)
             .then(response => {
-                console.log(response.data)
                 setEmailState(!emailState)
             })
             .catch(error => {
-                console.log(error);
+                console.log(error)
             });
     }
 
@@ -129,8 +126,8 @@ console.log('PARAMS',params)
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg row">
                         <div className="col-6 p-2">
                             <div className="row border-gray-400 overflow-hidden shadow-sm sm:rounded-lg m-2 p-2 col-12">
-                                <div className="col-4 font-bold">Location: </div>
-                                <div className="col-4">{cityName}</div>
+                                <div className="col-4 font-bold"></div>
+                                <div className="col-4 h3">{capitalizeFirst(cityName)}</div>
                                 <div className="font-bold col-4">
                                     <form onSubmit={addFavourite}>
                                         <input type="hidden" name="favourite" value={cityName}/>
@@ -152,7 +149,7 @@ console.log('PARAMS',params)
                                                         return  <div className="col-4" key={listValues['id']}>
                                                             <form onSubmit={handleSubmit}>
                                                                 <input type="hidden" name="city" value={listValues['data']}></input>
-                                                                <input type="submit" className="btn btn-outline-primary m-2" value={listValues['data']}/>
+                                                                <input type="submit" className="btn btn-outline-primary m-2" value={capitalizeFirst(listValues['data'])}/>
                                                             </form>
                                                         </div>
 
@@ -166,7 +163,7 @@ console.log('PARAMS',params)
                                 <div className="">
                                     <div className="border-gray-400 overflow-hidden shadow-sm sm:rounded-lg p-2 m-2">
                                         <form onSubmit={handleSubmit}>
-                                            <TextInput className="p-2" placeholder="Search Location...." name="city"></TextInput>
+                                            <TextInput className={'p-2'} placeholder="Search Location...." name={'city'}></TextInput>
                                             <input type="submit" className="btn btn-outline-primary m-2" value="Get Weather"/>
                                         </form>
                                         <div>
@@ -176,45 +173,47 @@ console.log('PARAMS',params)
                                 </div>
                             </div>
                         </div>
-                        <div className="w-50 col-6">
-                            <div className="col-12 row">
-                                <Tabs defaultIndex={0} onSelect={(index, last, event ) => console.log(event.target.value)}>
-                                    <TabList>
-                                        {
-                                            dayList.map(
-                                                (value, index) => (
-                                                        <Tab value={index} key={index}>
-                                                            {value}
-                                                        </Tab>
+                        <div className="col-6">
+                            <div className="row p-2">
+                                {typeof weatherData.daily !== 'undefined' &&
+                                    <Tabs defaultIndex={0} onSelect={(index, last, event ) => setTabIndex(event.target.value)}>
+                                        <TabList>
+                                            {
+                                                dayList.map(
+                                                    (value, index) => (
+                                                            <Tab value={index} key={index}>
+                                                                {value}
+                                                            </Tab>
+                                                    )
                                                 )
-                                            )
-                                        }
-                                    </TabList>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[0]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[1]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[2]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[3]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[4]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[5]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[6]}></WeatherDisplay>
-                                    </TabPanel>
-                                    <TabPanel>
-                                        <WeatherDisplay weatherData={data.weather.daily[7]}></WeatherDisplay>
-                                    </TabPanel>
-                                </Tabs>
+                                            }
+                                        </TabList>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[0]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[1]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[2]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[3]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[4]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[5]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[6]}></WeatherDisplay>
+                                        </TabPanel>
+                                        <TabPanel>
+                                            <WeatherDisplay weatherData={weatherData.daily[7]}></WeatherDisplay>
+                                        </TabPanel>
+                                    </Tabs>
+                                }
                             </div>
                         </div>
 
