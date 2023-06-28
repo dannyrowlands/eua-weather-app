03a6bac999cda11faa0dd296f5a321da8c14fdc6
dannyrowlands <dannyrowlands@users.noreply.github.com>
2023-06-28 16:05:46 +0100

    Tweaked weather layout.

--- resources/js/Components/WeatherDisplay.jsx
+++ resources/js/Components/WeatherDisplay.jsx
@@ -33,35 +33,37 @@ export default function WeatherDisplay(incomingData) {
             }
 
             <div className="font-bold">Detail</div>
-            <div className="col-12">
-                Sun Rise: {dayjs(sunrise).format('H:mm')}
-            </div>
-            <div className="col-12">
-                Sun Set: {dayjs(sunset).format('H:mm')}
-            </div>
-            <div className="col-12">
-                Temperature Day: {incomingData.weatherData.temp.day} C
-            </div>
-            <div className="col-12">
-                Temperature Evening: {incomingData.weatherData.temp.eve} C
-            </div>
-            <div className="col-12">
-                Pressure: {incomingData.weatherData.pressure}
-            </div>
-            <div className="col-12">
-                Rain: {incomingData.weatherData.rain}%
-            </div>
-            <div className="col-6">
-                Wind Speed: {incomingData.weatherData.wind_speed} mph
-            </div>
-            <div className="col-6">
-                Wind Direction: {incomingData.weatherData.wind_deg}
-            </div>
-            <div className="col-6">
-                Wind Gusts: {incomingData.weatherData.wind_gust} mph
-            </div>
-            <div className="col-6 pb-6">
-                UV Index: {incomingData.weatherData.uvi}
+            <div className="border-gray-400 overflow-hidden shadow-sm sm:rounded-lg p-2 m-2 flex-wrap row">
+                <div className="col-12">
+                    Sun Rise: {dayjs(sunrise).format('H:mm')}
+                </div>
+                <div className="col-12">
+                    Sun Set: {dayjs(sunset).format('H:mm')}
+                </div>
+                <div className="col-12">
+                    Temperature Day: {incomingData.weatherData.temp.day} C
+                </div>
+                <div className="col-12">
+                    Temperature Evening: {incomingData.weatherData.temp.eve} C
+                </div>
+                <div className="col-12">
+                    Pressure: {incomingData.weatherData.pressure}
+                </div>
+                <div className="col-12">
+                    Rain: {incomingData.weatherData.rain}%
+                </div>
+                <div className="col-6">
+                    Wind Speed: {incomingData.weatherData.wind_speed} mph
+                </div>
+                <div className="col-6">
+                    Wind Direction: {incomingData.weatherData.wind_deg}
+                </div>
+                <div className="col-6">
+                    Wind Gusts: {incomingData.weatherData.wind_gust} mph
+                </div>
+                <div className="col-6 pb-6">
+                    UV Index: {incomingData.weatherData.uvi}
+                </div>
             </div>
         </div>
     );
